7a60fa371f5b473ff914b8d91cedb690f8a484da
KimberlyFrostad <kimberly.frostad@gmail.com>
2024-02-03 19:41:43 -0500

    Modified for only successful sessions

--- sudoThing.pl
+++ sudoThing.pl
@@ -1,23 +1,21 @@
 #!/usr/bin/perl
 
-my $sudoCount = "/var/log/auth.log";
+my $sudoCount = '/var/log/auth.log';
 
-if (-e $sudoCount) {
-    open my $log_file, '<', $sudoCount or die "Error: Cannot open $sudoCount - $!";
+
+open my $log_file, '<', $sudoCount or die "Error: Cannot open $sudoCount - $!";
     
-    my $count = 0;
+my $count = 0;
     
-    while (my $line = <$log_file>) {
-        if ($line =~ "sudo:session") {
-            $count++;
-        }
-    }
-    
-    close $log_file;
-    
-    $count = $count / 2;
-    print $count;
-} else {
-    die "Error: $sudoCount not found";
+while ($line = <$log_file>) {
+	if ($line =~ /pam_unix\(sudo:session\): session opened/) {
+		$count++;
+	}
 }
+    
+close $log_file;
+print $count;
+
+
+
 
